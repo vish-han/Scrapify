1a05ea09070689174c582abb83405ed7c5ccb6d8
Vishal Chauhan <44545745+vish-han@users.noreply.github.com>
2022-09-14 14:16:45 +0530

    User Model Created

--- Server/controllers/UserController.js
+++ Server/controllers/UserController.js
@@ -1,3 +1,6 @@
-const register = () => {
+const Users=require('../models/UserModel')
+
+const register = (req, res) => {
+    const User=Users.create({name:req.name,email:req.email,password:req.password,address:req.address,isDealer:})
     
 }
