4edcb0ff06f7b58c0b2d19064acd53ece23a0f3f
Vishal Chauhan <44545745+vish-han@users.noreply.github.com>
2022-09-14 14:16:56 +0530

    UserModel done

--- Server/models/UserModel.js
+++ Server/models/UserModel.js
@@ -1,1 +1,13 @@
 const mongoose = require('mongoose');
+
+const User=mongoose.Schema({
+fullname:{type:"string",required:"true"},
+password:{type:"string",required:"true"},
+email:{type:"string",required:"true"},
+number:{type:"string",required:"true"},
+address:{type:"string",required:"true"},
+isHouseHolding:{type:"boolean",required:"true"},
+isDealer:{type:"boolean",required:"tru,e"}
+
+})
+export const Users = mongoose.model('User', User);
