2fec5c6cc0412159e00848a5bffc076546885e35
Vishal Chauhan <44545745+vish-han@users.noreply.github.com>
2022-09-30 23:39:13 +0530

    Image Added

--- Client/src/pages/Success.jsx
+++ Client/src/pages/Success.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
+import useWindowSize from 'react-use/lib/useWindowSize'
+import Confetti from 'react-confetti'
 
 const Success = () => {
+  const { width, height } = useWindowSize()
   return (
+    <>
+    <Confetti
+    width={width}
+    height={height}
+  />
     <div className='flex flex-col justify-center items-center m-12'>
         <img src="https://cdn-icons-png.flaticon.com/512/294/294432.png" alt=""  className='w-24'/>
         <h1 className='text-3xl'>Thanks for Chossing Scrapify</h1>
 
 </div>
-  )
+ </> )
 }
 
 export default Success
--- Server/controllers/UserController.js
+++ Server/controllers/UserController.js
@@ -10,16 +10,46 @@ const client = require("twilio")(accountSid, authToken);
 const register = async (req, res) => {
   console.log(req.body);
   const {name, email, address, phone, poster_path, userkind, isLocalAdmin} = req.body;
+//   const res1 = await fetch("https://libretranslate.com/translate", {
+// 	method: "POST",
+// 	body: JSON.stringify({
+// 		q: `Hey!! You have successfully registered in the SCRAPIFY, it's time to make a deal`,
+// 		source: "en",
+// 		target: "hi",
+// 		format: "text",
+// 		api_key: ""
+// 	}),
+// 	headers: { "Content-Type": "application/json" }
+// }).then(res1 => res1.json())
+// .then(json => console.log(json))
+// .catch(err => console.error('error:' + err));;
 
+
+// console.log(res1);
+client.messages
+.create({
+  from: "whatsapp:+14155238886",
+  to: `whatsapp:+91${phone}`,
+  body: `Hey ${name}!!!!!! You have successfully registered in the SCRAPIFY, it's time to make a deal`,
+
+
+})
+.then((message) => console.log(message.sid))
+.catch((err) => console.log(err))
+.done();
   client.messages
     .create({
-      body: "Hey!! You have successfully registered in the SCRAPIFY, it's time to make a deal",
       from: "whatsapp:+14155238886",
-      to: `whatsapp:${phone}`,
+      to: `whatsapp:+91${phone}`,
+     
+      mediaUrl:'https://img.freepik.com/free-vector/happy-tiny-people-near-huge-welcome-word-flat-illustration_74855-10808.jpg?w=1060&t=st=1664560946~exp=1664561546~hmac=46af893665d6adfd83244eea7d3576006926e39f97f274bf3ecceabcc9385cc5',
+      contentType :  ['image/jpg']
+
     })
     .then((message) => console.log(message.sid))
     .catch((err) => console.log(err))
-    .done();
+    .done();  
+  
 
   try {
     let checkEmail = await User.findOne({ email: email });
